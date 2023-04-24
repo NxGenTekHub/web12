28dd807cc8376853d618bc0a05a11f65e81edbc6
NxGenTekHub <nxgentekhub@gmail.com>
2023-04-24 15:40:13 -0500

    src/indec.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom';
 import App from './App';
 
 // CSS File Here
@@ -8,8 +9,10 @@ import "aos/dist/aos.css";
 import 'react-modal-video/scss/modal-video.scss';
 import './assets/scss/style.scss';
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
-
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
